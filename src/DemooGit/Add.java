704a0aba8c68575fbcf1d234cb81be7bf3bb5ce7
Yahya Khan <Yahya Khan@DESKTOP-V3I71I9>
2018-06-04 01:08:19 +0500

    ye b  lay lay

---
+++ src/DemooGit/Add.java
@@ -0,0 +1,17 @@
+package DemooGit;
+
+public class Add {
+	
+	int a ;
+	int b;
+	int result;
+	
+	
+	public int add(){
+		
+		result = a+b;
+		return result;
+		
+	}
+
+}
---
+++ src/DemooGit/MainAdd.java
@@ -0,0 +1,13 @@
+package DemooGit;
+
+public class MainAdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+
+	}
+
+}
